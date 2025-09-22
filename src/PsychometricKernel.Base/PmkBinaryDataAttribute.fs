d3dd99061bc628e054c4f5a974d7242b4bdf30ec
CoffeeLake <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2025-09-22 11:46:50 +0700

    gitignore config

---
+++ src/PsychometricKernel.Base/PmkBinaryDataAttribute.fs
@@ -0,0 +1,10 @@
+namespace PsychometricKernel.Base
+
+open System
+
+/// <summary>
+/// Tells loader use another schema for instanicating
+/// the implemented PMK extension.
+/// </summary>
+[<Class>]
+type PmkBinaryDataAtribute() = inherit Attribute()
